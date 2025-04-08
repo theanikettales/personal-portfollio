2c9ded65b758ed523bc9edc5ca88ac2bd2a01db1
theanikettales <theanikettales@gmail.com>
2025-04-08 23:27:14 +0530

    rendoring

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log(
+    "%cHey there, curious developer! 👀",
+    "color: #ff4081; font-size: 20px; font-weight: bold;"
+  );
+  
+  console.log(
+    "%cThis portfolio was handcrafted with love 💻 by Osiris.",
+    "color: #00bcd4; font-size: 16px;"
+  );
+  
+  console.log(
+    "%c🔗 Code: github.com/osirisdev/portfolio",
+    "color: #4caf50; font-size: 14px;"
+  );
+  
